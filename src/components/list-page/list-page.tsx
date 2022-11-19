cff995a30fdaad31d91f485256dd1d2293b728c2
denis-ttk-1975 <denis.ttk@gmail.com>
2022-11-19 23:15:20 +0300

    list layout done

--- src/components/list-page/list-page.tsx
+++ src/components/list-page/list-page.tsx
@@ -5,25 +5,44 @@ import { Circle } from '../ui/circle/circle';
 import { Button } from '../ui/button/button';
 
 import styles from './list-page.module.css';
-// import './list-page.css';
+import './list-page.css';
 
 export const ListPage: React.FC = () => {
-  const [word, setWord] = useState('hello');
+  const [word, setWord] = useState('12345');
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setWord(event.target.value);
   };
-  const arrayFromString = Array.from(word);
+  const listFromTask = Array.from(word);
 
   return (
     <SolutionLayout title='Связный список'>
-      <div className={`${styles.stringContentArea}`}>
-        <div className={`${styles.inputArea}`}>
-          <Input isLimitText={true} maxLength={11} extraClass={'string'} onChange={handleChange} />
-          <Button text={'Развернуть'} extraClass={'button-style'} />
+      <div className={`${styles.listContentArea}`}>
+        <div className={`${styles.verticalInputForm}`}>
+          <div className={`${styles.inputArea}`}>
+            <Input isLimitText={true} type={'number'} max={4} extraClass={'input-style'} onChange={handleChange} />
+            <Button text={'Добавить в head'} extraClass={'button-style-middle'} />
+            <Button text={'Добавить в tail'} extraClass={'button-style-middle'} />
+            <Button text={'Удалить из head'} extraClass={'button-style-middle'} />
+            <Button text={'Удалить из tail'} extraClass={'button-style-middle'} />
+          </div>
+          <div className={`${styles.inputArea}`}>
+            <Input placeholder={'Введите индекс'} isLimitText={false} type={'number'} extraClass={'input-style'} onChange={handleChange} />
+            <Button text={'Добавить по индексу'} extraClass={'button-style-long'} />
+            <Button text={'Удалить по индексу'} extraClass={'button-style-long'} />
+          </div>
         </div>
         <div className={`${styles.circleArea}`}>
-          {arrayFromString.map((elem) => {
-            return <Circle letter={elem} />;
+          {listFromTask.map((elem, key, array) => {
+            return (
+              <div className={`${styles.countedCircleWithUnderText}`}>
+                <div className={`${styles.countedCircle}`}>
+                  <p>{key === 1 ? 'head' : ' '}</p>
+                  <Circle letter={elem} />
+                  <p>{key}</p>
+                </div>
+                <p>{key === 4 ? 'tail' : ' '}</p>
+              </div>
+            );
           })}
         </div>
       </div>
