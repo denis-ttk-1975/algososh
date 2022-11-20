300a0585e7c70ef300779bc21fa4add8588603fc
denis-ttk-1975 <denis.ttk@gmail.com>
2022-11-20 13:58:45 +0300

    button-handlr added for string; algorithm to do

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ChangeEvent } from 'react';
+import React, { useState, ChangeEvent, MouseEventHandler, MouseEvent } from 'react';
 import { SolutionLayout } from '../ui/solution-layout/solution-layout';
 import { Input } from '../ui/input/input';
 import { Circle } from '../ui/circle/circle';
@@ -7,23 +7,34 @@ import { Button } from '../ui/button/button';
 import styles from './string.module.css';
 import './string.css';
 
+let arrayFromString: string[] = [];
+
 export const StringComponent: React.FC = () => {
-  const [word, setWord] = useState('hello');
+  const [word, setWord] = useState('');
+  const [isStringTurning, setStringTurning] = useState(false);
+
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setWord(event.target.value);
   };
-  const arrayFromString = Array.from(word);
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    setStringTurning(!isStringTurning);
+    arrayFromString = Array.from(word);
+    console.log('arrayFromString1: ', arrayFromString);
+    setStringTurning(!isStringTurning);
+  };
+  console.log('arrayFromString2: ', arrayFromString);
 
   return (
     <SolutionLayout title='Строка'>
       <div className={`${styles.stringContentArea}`}>
         <div className={`${styles.inputArea}`}>
           <Input isLimitText={true} maxLength={11} extraClass={'input-style'} onChange={handleChange} />
-          <Button text={'Развернуть'} extraClass={'button-style'} />
+          <Button text={'Развернуть'} extraClass={'button-style'} onClick={handleClick} />
         </div>
         <div className={`${styles.circleArea}`}>
-          {arrayFromString.map((elem) => {
-            return <Circle letter={elem} />;
+          <div></div>
+          {arrayFromString.map((elem, index) => {
+            return <Circle letter={elem} key={index} />;
           })}
         </div>
       </div>
