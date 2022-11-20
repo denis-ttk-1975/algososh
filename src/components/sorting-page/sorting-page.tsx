10139c50e53b3b306be79950b713d09e7d016df7
denis-ttk-1975 <denis.ttk@gmail.com>
2022-11-20 12:36:52 +0300

    sorting-array layout done

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -1,29 +1,35 @@
 import React, { useState, ChangeEvent } from 'react';
 import { SolutionLayout } from '../ui/solution-layout/solution-layout';
-import { Input } from '../ui/input/input';
-import { Circle } from '../ui/circle/circle';
+import { RadioInput } from '../ui/radio-input/radio-input';
+import { Column } from '../ui/column/column';
 import { Button } from '../ui/button/button';
+import { Direction } from '../../types/direction';
+import { ElementStates } from '../../types/element-states';
 
 import styles from './sorting-page.module.css';
-// import './sorting-page.css';
+import './sorting-page.css';
 
 export const SortingPage: React.FC = () => {
   const [word, setWord] = useState('hello');
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setWord(event.target.value);
   };
-  const arrayFromString = Array.from(word);
+  const arrayForSorting = [55, 76, 23, 45, 99];
 
   return (
     <SolutionLayout title='Сортировка массива'>
-      <div className={`${styles.stringContentArea}`}>
+      <div className={`${styles.sortingContentArea}`}>
         <div className={`${styles.inputArea}`}>
-          <Input isLimitText={true} maxLength={11} extraClass={'string'} onChange={handleChange} />
-          <Button text={'Развернуть'} extraClass={'button-style'} />
+          <RadioInput label={'Выбор'} extraClass={'margin-right-40'} checked name='method' />
+          <RadioInput label={'Пузырек'} extraClass={'margin-right-52'} name='method' />
+
+          <Button sorting={Direction.Ascending} text={'По возрастанию'} extraClass={''} />
+          <Button sorting={Direction.Descending} text={'По убыванию'} extraClass={''} />
+          <Button text={'Новый массив'} extraClass={'margin-left-68'} />
         </div>
-        <div className={`${styles.circleArea}`}>
-          {arrayFromString.map((elem) => {
-            return <Circle letter={elem} />;
+        <div className={`${styles.columnArea}`}>
+          {arrayForSorting.map((elem) => {
+            return <Column index={elem} />;
           })}
         </div>
       </div>
