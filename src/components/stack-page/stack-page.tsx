8a6d59c16cf6698f2dfb089556f54008f57cc35b
denis-ttk-1975 <denis.ttk@gmail.com>
2022-12-04 12:55:55 +0300

    digits added to stack

--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -1,35 +1,67 @@
-import React, { useState, ChangeEvent } from 'react';
+import React, { useState, ChangeEvent, useRef, useEffect, MouseEvent } from 'react';
 import { SolutionLayout } from '../ui/solution-layout/solution-layout';
 import { Input } from '../ui/input/input';
 import { Circle } from '../ui/circle/circle';
 import { Button } from '../ui/button/button';
 
+import { DELAY_IN_MS, SHORT_DELAY_IN_MS } from './../../constants/delays';
+
 import styles from './stack-page.module.css';
 import './stack-page.css';
 
+let stackFromTask: number[] = [];
+
 export const StackPage: React.FC = () => {
-  const [word, setWord] = useState('1234');
+  const timerRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+
+  // clear timer Timeout when unmounted to prevent memory leak
+
+  useEffect(() => {
+    return () => {
+      if (timerRef.current) {
+        clearTimeout(timerRef.current);
+      }
+    };
+  }, []);
+
+  const [word, setWord] = useState<number | null>(null);
+
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setWord(event.target.value);
+    setWord(+event.target.value);
   };
-  const stackFromTask = Array.from(word);
+
+  const handleAddClick = (event: MouseEvent<HTMLButtonElement>) => {
+    if (word) {
+      stackFromTask.push(word);
+    }
+  };
+
+  const handleDeleteClick = (event: MouseEvent<HTMLButtonElement>) => {
+    if (stackFromTask.length) {
+      stackFromTask.pop();
+    }
+  };
+
+  const handlePurgeClick = (event: MouseEvent<HTMLButtonElement>) => {
+    stackFromTask = [];
+  };
 
   return (
     <SolutionLayout title='Стек'>
       <div className={`${styles.stackContentArea}`}>
         <div className={`${styles.inputArea}`}>
-          <Input isLimitText={true} type={'number'} max={4} extraClass={'input-style'} onChange={handleChange} />
-          <Button text={'Добавить'} extraClass={''} />
-          <Button text={'Удалить'} extraClass={''} />
+          <Input isLimitText={true} type={'number'} max={9999} extraClass={'input-style'} onChange={handleChange} />
+          <Button text={'Добавить'} extraClass={''} onClick={handleAddClick} />
+          <Button text={'Удалить'} extraClass={''} onClick={handleDeleteClick} />
 
-          <Button text={'Очистить'} extraClass={'margin-left-68'} />
+          <Button text={'Очистить'} extraClass={'margin-left-68'} onClick={handlePurgeClick} />
         </div>
         <div className={`${styles.circleArea}`}>
           {stackFromTask.map((elem, key, array) => {
             return (
-              <div className={`${styles.countedCircle}`}>
+              <div className={`${styles.countedCircle}`} key={key}>
                 <p>{key !== array.length - 1 ? ' ' : 'top'}</p>
-                <Circle letter={elem} />
+                <Circle letter={String(elem)} />
                 <p>{key}</p>
               </div>
             );
