add306ebcbdc4ccbefbb0fc47242a92bbeeff40f
denis-ttk-1975 <denis.ttk@gmail.com>
2022-12-07 12:21:29 +0300

    queue reformed to class

---
+++ src/components/queue-page/queue-class.ts
@@ -0,0 +1,58 @@
+export class Queue {
+  stack: string[];
+  head: number | null;
+  tail: number | null;
+
+  constructor() {
+    this.stack = [' ', ' ', ' ', ' ', ' ', ' ', ' '];
+    this.head = null;
+    this.tail = null;
+  }
+
+  add(value: string) {
+    if (this.tail === null || this.head === null) {
+      this.stack[0] = value;
+      this.head = 0;
+      this.tail = 0;
+      return this;
+    }
+    if ((this.head === 0 && this.tail === 6) || this.tail + 1 === this.head) {
+      alert('В данном примере очередь ограничена 7 элементами. Пожалуйста, удалите один или несколько элементов, чтобы добавить новый элемент.');
+      return this;
+    }
+    if (this.tail === 6) {
+      this.stack[0] = value;
+      this.tail = 0;
+      return this;
+    }
+    this.stack[this.tail + 1] = value;
+    this.tail = this.tail + 1;
+    return this;
+  }
+
+  delete() {
+    if (this.tail === null || this.head === null) {
+      return this;
+    }
+    if (this.head === this.tail) {
+      this.clear();
+      return this;
+    }
+
+    if (this.head === 6) {
+      this.stack[0] = ' ';
+      this.head = 0;
+      return this;
+    }
+    this.stack[this.head] = ' ';
+    this.head = this.head + 1;
+    return this;
+  }
+
+  clear() {
+    this.stack = [' ', ' ', ' ', ' ', ' ', ' ', ' '];
+    this.head = null;
+    this.tail = null;
+    return this;
+  }
+}
--- src/components/queue-page/queue-page.tsx
+++ src/components/queue-page/queue-page.tsx
@@ -7,16 +7,22 @@ import { ElementStates } from '../../types/element-states';
 
 import { DELAY_IN_MS, SHORT_DELAY_IN_MS } from './../../constants/delays';
 
+import { Queue } from './queue-class';
+
 import styles from './queue-page.module.css';
 
-let queueArray: string[] = [];
-let queueHead = 0;
-let queueTail = 0;
+// let queueArray: string[] = [];
+// let queueHead = 0;
+// let queueTail = 0;
 
 export const QueuePage: React.FC = () => {
   const [word, setWord] = useState('');
 
-  const [queueForRender, setQueueForRender] = useState<string[]>([' ', ' ', ' ', ' ', ' ', ' ', ' ']);
+  const queueForRender = useRef(new Queue());
+
+  // const queueForRender = queue.current;
+
+  // const [queueForRender, setQueueForRender] = useState<string[]>([' ', ' ', ' ', ' ', ' ', ' ', ' ']);
 
   const [animation, setAnimation] = useState<'head' | 'tail' | null>(null);
 
@@ -43,94 +49,98 @@ export const QueuePage: React.FC = () => {
   };
 
   const handleAddClick = (event: MouseEvent<HTMLButtonElement>) => {
-    if (queueArray.length < 7) {
-      if (word) {
-        let bulletArray: string[] = [];
+    // if (queueArray.length < 7) {
+    //   if (word) {
+    //     let bulletArray: string[] = [];
 
-        queueArray = [...queueArray, word];
+    //     queueArray = [...queueArray, word];
 
-        if (queueHead && queueHead < 6) {
-          queueHead = queueHead;
-        } else {
-          queueHead = 0;
-        }
-        queueTail = (queueHead + queueArray.length - 1) % 7;
+    //     if (queueHead && queueHead < 6) {
+    //       queueHead = queueHead;
+    //     } else {
+    //       queueHead = 0;
+    //     }
+    //     queueTail = (queueHead + queueArray.length - 1) % 7;
 
-        for (let i = 0, j = queueArray.length - 1; i <= j; i++) {
-          let index;
-          if (i + queueHead > 6) {
-            index = i + queueHead - 7;
-          } else {
-            index = i + queueHead;
-          }
-          bulletArray[index] = !!queueArray[i] ? queueArray[i] : ' ';
-        }
-        for (let i = 0; i < 7; i++) {
-          if (!bulletArray[i]) {
-            bulletArray[i] = ' ';
-          }
-        }
-        setQueueForRender(() => [...bulletArray]);
+    //     for (let i = 0, j = queueArray.length - 1; i <= j; i++) {
+    //       let index;
+    //       if (i + queueHead > 6) {
+    //         index = i + queueHead - 7;
+    //       } else {
+    //         index = i + queueHead;
+    //       }
+    //       bulletArray[index] = !!queueArray[i] ? queueArray[i] : ' ';
+    //     }
+    //     for (let i = 0; i < 7; i++) {
+    //       if (!bulletArray[i]) {
+    //         bulletArray[i] = ' ';
+    //       }
+    //     }
+    //     setQueueForRender(() => [...bulletArray]);
 
-        setWord('');
-        setAnimation('tail');
-      }
-    } else alert('В данном примере очередь ограничена 7 элементами. Пожалуйста, удалите один или несколько элементов, чтобы добавить новый элемент.');
+    queueForRender.current.add(word);
+
+    setWord('');
+    setAnimation('tail');
   };
+  //   } else alert('В данном примере очередь ограничена 7 элементами. Пожалуйста, удалите один или несколько элементов, чтобы добавить новый элемент.');
+  // };
 
   const handleDeleteClick = (event: MouseEvent<HTMLButtonElement>) => {
-    if (queueArray.length <= 1) {
-      let bulletArray: string[] = [];
-      for (let i = 0; i < 7; i++) {
-        if (!bulletArray[i]) {
-          bulletArray[i] = ' ';
-        }
-      }
-      setQueueForRender([...bulletArray]);
-      queueArray = [];
-      queueHead = 0;
-      queueTail = 0;
-    } else {
-      let bulletArray: string[] = [];
+    // if (queueArray.length <= 1) {
+    //   let bulletArray: string[] = [];
+    //   for (let i = 0; i < 7; i++) {
+    //     if (!bulletArray[i]) {
+    //       bulletArray[i] = ' ';
+    //     }
+    //   }
+    //   setQueueForRender([...bulletArray]);
+    //   queueArray = [];
+    //   queueHead = 0;
+    //   queueTail = 0;
+    // } else {
+    //   let bulletArray: string[] = [];
 
-      queueArray.shift();
-      if (queueHead < 6) {
-        queueHead = queueHead + 1;
-      } else {
-        queueHead = 0;
-      }
-      queueTail = (queueHead + queueArray.length - 1) % 7;
+    //   queueArray.shift();
+    //   if (queueHead < 6) {
+    //     queueHead = queueHead + 1;
+    //   } else {
+    //     queueHead = 0;
+    //   }
+    //   queueTail = (queueHead + queueArray.length - 1) % 7;
 
-      for (let i = 0, j = queueArray.length - 1; i <= j; i++) {
-        let index;
-        if (i + queueHead > 6) {
-          index = i + queueHead - 7;
-        } else {
-          index = i + queueHead;
-        }
-        bulletArray[index] = queueArray[i];
-      }
-      for (let i = 0; i < 7; i++) {
-        if (!bulletArray[i]) {
-          bulletArray[i] = ' ';
-        }
-      }
-      setQueueForRender([...bulletArray]);
-      setAnimation('head');
-    }
+    //   for (let i = 0, j = queueArray.length - 1; i <= j; i++) {
+    //     let index;
+    //     if (i + queueHead > 6) {
+    //       index = i + queueHead - 7;
+    //     } else {
+    //       index = i + queueHead;
+    //     }
+    //     bulletArray[index] = queueArray[i];
+    //   }
+    //   for (let i = 0; i < 7; i++) {
+    //     if (!bulletArray[i]) {
+    //       bulletArray[i] = ' ';
+    //     }
+    //   }
+    //   setQueueForRender([...bulletArray]);
+    queueForRender.current.delete();
+    setAnimation('head');
   };
 
   const handlePurgeClick = (event: MouseEvent<HTMLButtonElement>) => {
-    let bulletArray: string[] = [];
-    for (let i = 0; i < 7; i++) {
-      if (!bulletArray[i]) {
-        bulletArray[i] = ' ';
-      }
-    }
-    setQueueForRender([...bulletArray]);
-    queueArray = [];
-    queueHead = 0;
-    queueTail = 0;
+    // let bulletArray: string[] = [];
+    // for (let i = 0; i < 7; i++) {
+    //   if (!bulletArray[i]) {
+    //     bulletArray[i] = ' ';
+    //   }
+    // }
+    // setQueueForRender([...bulletArray]);
+    // queueArray = [];
+    // queueHead = 0;
+    // queueTail = 0;
+    queueForRender.current.clear();
+    setAnimation('tail');
   };
 
   return (
@@ -139,21 +149,23 @@ export const QueuePage: React.FC = () => {
         <div className={`${styles.inputArea}`}>
           <Input isLimitText={true} type={'text'} maxLength={4} extraClass={'input-style'} onChange={handleChange} value={word} />
           <Button text={'Добавить'} extraClass={''} onClick={handleAddClick} disabled={!word} />
-          <Button text={'Удалить'} extraClass={''} onClick={handleDeleteClick} disabled={!queueArray.length} />
+          <Button text={'Удалить'} extraClass={''} onClick={handleDeleteClick} disabled={queueForRender.current.tail === null} />
 
-          <Button text={'Очистить'} extraClass={'margin-left-68'} onClick={handlePurgeClick} disabled={!queueArray.length} />
+          <Button text={'Очистить'} extraClass={'margin-left-68'} onClick={handlePurgeClick} disabled={queueForRender.current.tail === null} />
         </div>
         <div className={`${styles.circleArea}`}>
-          {queueForRender.map((elem, key, array) => {
+          {queueForRender.current.stack.map((elem, key, array) => {
             return (
               <div className={`${styles.countedCircleWithUnderText}`} key={key}>
                 <div className={`${styles.countedCircle}`}>
                   <Circle
                     letter={String(elem)}
-                    head={key === queueHead && queueArray.length ? 'head' : ' '}
-                    tail={key === queueTail && queueArray.length ? 'tail' : ' '}
+                    head={key === queueForRender.current.head ? 'head' : ' '}
+                    tail={key === queueForRender.current.tail ? 'tail' : ' '}
                     index={key}
-                    state={(animation === 'head' && key === queueHead) || (animation === 'tail' && key === queueTail) ? ElementStates.Changing : ElementStates.Default}
+                    state={
+                      (animation === 'head' && key === queueForRender.current.head) || (animation === 'tail' && key === queueForRender.current.tail) ? ElementStates.Changing : ElementStates.Default
+                    }
                   />
                 </div>
               </div>
