042a2678cacdff5e71bc20b6cfd25b5b5278396f
denis-ttk-1975 <denis.ttk@gmail.com>
2022-12-10 21:54:20 +0300

    cypress installed and first test done

---
+++ cypress/e2e/app.spec.cy.ts
@@ -0,0 +1,18 @@
+describe('empty spec', () => {
+  it('passes', () => {
+    cy.visit('https://example.cypress.io');
+  });
+});
+
+describe('spec visit', () => {
+  it('Does not do much!', () => {
+    expect(true).to.equal(true);
+  });
+});
+
+describe('service is available', function () {
+  it('should be available on localhost:3000', function () {
+    cy.visit('http://localhost:3000');
+    cy.contains('МБОУ АЛГОСОШ');
+  });
+});
