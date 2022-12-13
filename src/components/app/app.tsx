3d6c5c8f36c8dbdefb0e7225a5955aa808fe5cdd
denis-ttk-1975 <denis.ttk@gmail.com>
2022-12-13 19:41:06 +0300

    app.css excluded from app.tsx

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -8,11 +8,13 @@ import { StringComponent } from '../string/string';
 import { SortingPage } from '../sorting-page/sorting-page';
 import { StackPage } from '../stack-page/stack-page';
 
-import './app.css';
+import styles from './app.module.css';
+
+// import './app.css';
 
 function App() {
   return (
-    <div className='app'>
+    <div className={`${styles.app}`}>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<MainPage />} />
