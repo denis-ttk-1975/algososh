2423637f5348d448a7cb2b4e47ddbadf31461c60
denis-ttk-1975 <denis.ttk@gmail.com>
2022-12-13 14:58:43 +0300

    loader for stack added

--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -20,9 +20,12 @@ export const StackPage: React.FC = () => {
 
   const [animation, setAnimation] = useState(false);
 
+  const [button, setButton] = useState<'add' | 'delete' | 'purge' | null>(null);
+
   useEffect(() => {
     timerRef.current = setTimeout(() => {
       setAnimation(false);
+      setButton(null);
     }, SHORT_DELAY_IN_MS);
   }, [animation]);
 
@@ -48,6 +51,7 @@ export const StackPage: React.FC = () => {
         stackForRender.current.add(word);
         setWord('');
         setAnimation(true);
+        setButton('add');
       }
     } else alert('В данном примере стек ограничен 20 элементами. Пожалуйста, удалите один или несколько элементов, чтобы добавить новый элемент.');
   };
@@ -56,15 +60,18 @@ export const StackPage: React.FC = () => {
     if (stackForRender.current.stack.length === 1) {
       stackForRender.current.clear();
       setAnimation(true);
+      setButton('delete');
     } else {
       stackForRender.current.delete();
       setAnimation(true);
+      setButton('delete');
     }
   };
 
   const handlePurgeClick = (event: MouseEvent<HTMLButtonElement>) => {
     stackForRender.current.clear();
     setAnimation(true);
+    setButton('purge');
   };
 
   return (
@@ -72,10 +79,10 @@ export const StackPage: React.FC = () => {
       <div className={`${styles.stackContentArea}`}>
         <div className={`${styles.inputArea}`}>
           <Input isLimitText={true} type={'text'} maxLength={4} extraClass={'input-style'} onChange={handleChange} value={word} data-testid='input' />
-          <Button text={'Добавить'} extraClass={''} onClick={handleAddClick} name={'add'} value={'add'} disabled={!word} data-testid='add' />
-          <Button text={'Удалить'} extraClass={''} onClick={handleDeleteClick} disabled={!stackForRender.current.stack.length} data-testid='delete' />
+          <Button text={'Добавить'} extraClass={''} onClick={handleAddClick} name={'add'} value={'add'} disabled={!word} data-testid='add' isLoader={button === 'add'} />
+          <Button text={'Удалить'} extraClass={''} onClick={handleDeleteClick} disabled={!stackForRender.current.stack.length} data-testid='delete' isLoader={button === 'delete'} />
 
-          <Button text={'Очистить'} extraClass={'margin-left-68'} onClick={handlePurgeClick} disabled={!stackForRender.current.stack.length} data-testid='purge' />
+          <Button text={'Очистить'} extraClass={'margin-left-68'} onClick={handlePurgeClick} disabled={!stackForRender.current.stack.length} data-testid='purge' isLoader={button === 'purge'} />
         </div>
         <div className={`${styles.circleArea}`}>
           {stackForRender.current.stack.map((elem, key, array) => {
